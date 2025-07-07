8a478457000ef1a112abf0d71c19b729ba70d907
Andy94m <m.andresmontes@gmail.com>
2025-07-07 16:39:26 -0300

    to top y css

--- funciones.js
+++ funciones.js
@@ -1,11 +1,34 @@
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('href');
         const targetElement = document.querySelector(targetId);
-        window.scrollTo({
-            top: targetElement.offsetTop - 10,  // Ajusta el valor para que no quede demasiado pegado al borde
-            behavior: 'smooth'
-        });
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 10,
+                behavior: 'smooth'
+            });
+        }
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toTopBtn = document.getElementById('toTopBtn');
+
+    if (toTopBtn) {
+        toTopBtn.style.display = "none";
+
+        window.addEventListener('scroll', function () {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                toTopBtn.style.display = "block";
+            } else {
+                toTopBtn.style.display = "none";
+            }
+        });
+
+        toTopBtn.addEventListener('click', function () {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+    }
 });
